d703f5f765957837fb4e52aebb62da6d7a59de3c
Kalkidanalemaye <59233048+Kalkidanalemaye@users.noreply.github.com>
2020-07-25 12:25:24 -0400

    storyboard

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
+import pandas as pd
+df = pd.read_csv ('cleaned_aq.csv')
+df.to_json ('cleaned_aq.json')
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
