c4877fac6da5ea4f1c0419513569553619653a6f
Osama Ali <mohd.osamaali@gmail.com>
2020-08-09 12:36:10 -0400

    fixing columns in ML

---
+++ Python Code/config.py
@@ -0,0 +1,6 @@
+# Postgres username, password, and database name
+POSTGRES_ADDRESS = 'team7.cpliq65f81hf.ca-central-1.rds.amazonaws.com' ## INSERT YOUR DB ADDRESS 
+POSTGRES_PORT = '5432'
+POSTGRES_USERNAME = 'team7'## POSTGRES USERNAME
+POSTGRES_PASSWORD = 'password' ##  PASSWORD 
+POSTGRES_DBNAME = 'modelingPD' ## DATABASE NAME
